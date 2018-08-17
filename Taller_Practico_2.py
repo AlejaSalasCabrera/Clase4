42bf533b0248a2cec55edf6cc61edc559103145c
aleja <aleja.20012010@gmail.com>
2018-08-17 14:43:21 -0500

    taller

--- Taller_Practico_2.py
+++ Taller_Practico_2.py
@@ -1,48 +1,97 @@
+dcon = {}
+duser = {}
+dpost = {}
+contenidouser = {'user':duser, 'post':dpost, 'comment':dcon}
+
+
+
 class Comment:
     id_com = 0
     comentario = ""
-    fecha_com = ""
+    id_post = 0
 
-    def __init__(self, id_com, comentario, fecha_com):
+
+    '''def __init__(self, id_com, comentario):
         self.id_com = id_com
-        self.comentario = comentario
-        self.fecha_com = fecha_com
+        self.comentario = comentario'''
+
 
     def Crear_Comentario(self):
-        self.id_com = input("ingrese su id de comentario:")
-        self.comentario = input("ingrese su comentario:")
-        self.fecha_com = input("ingrese la fech de publicain:")
+        self.id_post = input("ingre id post")
+        for post in contenidouser['post']:
+            if post == self.id_post:
+                self.id_com = input("ingrese su id de comentario: ")
+                self.comentario = input("ingrese su comentario: ")
+                dcon[self.id_com] = {self.id_post:self.comentario}
+            else:
+                print("El post no existe")
+
+
 
 class Post(Comment):
     id_post = 0
     titulo = ""
-    fecha_post = ""
+    id_user = 0
 
-    def __init__(self, id_com, comentario, fecha_com, id_post, titulo, fecha_post):
-        Comment.__init__(self, id_com, comentario, fecha_com)
+    '''def __init__(self, id_com, comentario, id_post, titulo):
+        Comment.__init__(self, id_com, comentario)
         self.id_post = id_post
-        self.titulo = titulo
-        self.fecha_post = fecha_post
+        self.titulo = titulo'''
+
 
     def Crear_Post(self):
-        self.id_post = input("ingrese su id de post:")
-        self.titulo = input("ingrese su titulo de post:")
-        self.fecha_post = input("ingrese su fecha de publicacion de post: ")
+        self.id_user = input("ingrese su id de usuario: ")
+        for us in contenidouser['user']:
+            if us == self.id_user:
+                self.id_post = input("ingrese su id de post:")
+                self.titulo = input("ingrese su titulo de post:")
+
+                dpost[self.id_post] = {self.id_user : self.titulo}
+
+            else:
+                print("el usuario no existe: ")
 
 
 class User(Post):
     id_user = 0
     nombre = ""
-    contraseña = ""
 
-    def __init__(self, id_user, nombre, contraseña):
+    '''def __init__(self, id_user, nombre):
         self.id_user = id_user
-        self.nombre = nombre
-        self.contraseña = contraseña
+        self.nombre = nombre'''
 
     def Crear_User(self):
 
         self.id_user = input("ingrese su identificacion: ")
         self.nombre = input("ingrese su nombre: ")
-        self.contraseña = input("ingrese su contraseña: ")
+
+        duser[self.id_user] = self.nombre
+
+
+print("Menu de opciones")
+print("1. crear usuario")
+print("2. crear post")
+print("3. crear comentario")
+print("4. buscar")
+
+opc =int(input("Digite la opcion: "))
+
+while opc != 5:
+
+    usuario = User()
+    if opc == 1:
+        usuario.Crear_User()
+
+    elif opc == 2:
+
+        usuario.Crear_Post()
+    elif opc == 3:
+        usuario.Crear_Comentario()
+    elif opc == 4:
+        print(contenidouser)
+    else:
+
+        print("error")
+
+    opc =int(input("Digite la opcion: "))
 
